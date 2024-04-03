0f33e10432053a66544f00b6abf9edb2baba0058
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2024-04-03 18:51:44 +0900

    Fix: document is not defined error with SSR

--- src/app/optionButtons.tsx
+++ src/app/optionButtons.tsx
@@ -1,21 +1,26 @@
 
 import styles from './optionButtons.module.sass'
 import Image from 'next/image'
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import {ConfigContext} from '@/provider/ConfigProvider'
 type Props = {}
 
 // if it uses 'slot', you need specify {children}: React.PropsWithChildren<Props>
 function optionButtons({}: Props) {
   // ... some codes
+  const [isClient, setIsClient] = useState(false)
   const {isFullscreen, setIsFullscreen, setIsOpenConfig} = useContext(ConfigContext)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
   function triggerFullscreen () {
     setIsFullscreen(!isFullscreen)
   }
   return <>
     <div className={styles.container}>
-      <div className={styles.config} onClick={() => setIsOpenConfig(true)}><Image src={`/icon_config.svg`} width={40} height={40} alt="Config" /></div>
-      {document.fullscreenEnabled &&
+      <div className={styles.config} onClick={() => setIsOpenConfig(true)}><Image src={`/icon_config.svg`} width={40} height={40} alt="Config" priority /></div>
+      { isClient && document.fullscreenEnabled &&
         <div className={styles.fullscreen} onClick={triggerFullscreen}>
           { isFullscreen && <Image src={`/icon_defaultScreen.svg`} width={40} height={40} alt="exitFullScreen" />}
           { !isFullscreen && <Image src={`/icon_fullScreen.svg`} width={40} height={40} alt="requestFullScreen" />}
