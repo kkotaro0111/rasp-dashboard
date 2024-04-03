a6e571c3065dcd3412288f8a1a1d636c99143b3d
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2024-04-03 18:52:50 +0900

    Change: to adjust layout

--- src/app/dailyWeather.tsx
+++ src/app/dailyWeather.tsx
@@ -33,7 +33,6 @@ function weather({index}: Props) {
           </div>
           <div className={styles.temperature}>
             <div className={styles.maxTemp}>{Math.round(weatherData.daily.temperature_2m_max[index] * 10) / 10}°</div>
-            |
             <div className={styles.minTemp}>{Math.round(weatherData.daily.temperature_2m_min[index] * 10) / 10}°</div>
           </div>
           <div className={styles.props}>
